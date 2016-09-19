9a6f01d44a07dd66ab01cb31fc69531111ad2f7d
Adam Sharp <adsharp@me.com>
2016-09-19 14:11:25 -0400

    Add PropertyAction
    PropertyAction encapsulates the pattern where an Action should be
    enabled only if its input is non-nil.
    
    Ideally this could just be a convenience initialiser on `Action`:
    
        extension Action where Input == Void {
          // ...
        }
    
    But unfortunately this triggers Swift's "same-type requirement makes
    generic parameter non-generic" error.

--- Tests/ReactiveSwiftTests/ActionSpec.swift
+++ Tests/ReactiveSwiftTests/ActionSpec.swift
@@ -138,5 +138,33 @@ class ActionSpec: QuickSpec {
 				}
 			}
 		}
+
+		describe("PropertyAction") {
+			it("executes the action with the property's current value") {
+				let input = MutableProperty(0)
+				let action = PropertyAction<Int, NoError>(input: input) { SignalProducer(value: $0) }
+
+				var values: [Int] = []
+				action.values.observeValues { values.append($0) }
+
+				input.value = 1
+				action.apply().start()
+				input.value = 2
+				action.apply().start()
+				input.value = 3
+				action.apply().start()
+
+				expect(values) == [1, 2, 3]
+			}
+
+			it("is disabled if the property is nil") {
+				let input = MutableProperty<Int?>(1)
+				let action = PropertyAction<Int, NoError>(input: input) { SignalProducer(value: $0) }
+
+				expect(action.isEnabled.value) == true
+				input.value = nil
+				expect(action.isEnabled.value) == false
+			}
+		}
 	}
 }
